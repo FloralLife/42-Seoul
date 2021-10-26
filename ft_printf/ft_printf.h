1993dc3bdba812a838e4640b52ab2b1916a73fcd
FloralLife <ocard9611@naver.com>
2021-10-26 22:09:03 +0900

    ft_printf start

---
+++ ft_printf/ft_printf.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yunolee <yunolee@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/24 14:45:59 by yunolee           #+#    #+#             */
+/*   Updated: 2021/10/24 14:45:59 by yunolee          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdarg.h>
+
+# include <stdio.h>
+
+
+int ft_printf(const char *, ...);
+
+
+#endif
